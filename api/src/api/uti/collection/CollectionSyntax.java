c8b3997d3f578e7c1b22cc63030fbdaf92bfbcc9
jkpark3070 <jkpark3070@gmail.com>
2015-05-31 22:41:35 +0900

    sun-end

--- api/src/api/uti/collection/CollectionSyntax.java
+++ api/src/api/uti/collection/CollectionSyntax.java
@@ -5,7 +5,16 @@ import java.util.Map;
 import java.util.Set;
 
 /*
-컬렉션 (Collection)
+컬렉션 프레임워크(Collection)
+/*
+ 다수의 데이터를 쉽게 처리할 수 있는 표준화된 
+ 방법을 제공하는 클래스들.
+ 3가지 타입으로 기능을 요약하고 3개의 인터페이스
+ List, Set, Map 으로 나누어 구현함.
+ 후에 List 와 Set 의 공통부분을 뽑아서 새로운
+ 인터페이스인 Collection 을 정의함.
+ * */
+/*
 - 요소(element) 라고 불리는 객체들의 집합
 - 객체들의 컨테이너
 - 요소들의 개수에 따라 동적으로 크기(size) 가 변한다.
